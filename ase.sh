ec1a298df00907c3a0f681f6bba44b4cfcb2f371
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-04-17 18:56:23 +0900

    Renamed script

--- ase.sh
+++ ase.sh
@@ -1,3 +1,3 @@
-( groovy asespotter2011.groovy > ~/t/ase.txt ; grep "\[" ~/t/ase.txt ; zenity --info --text 'Done') &
+( groovy asespotter.groovy > ~/t/ase.txt ; grep "\[" ~/t/ase.txt ; zenity --info --text 'Done') &
 sleep 5
 tail -f ~/t/ase.txt
