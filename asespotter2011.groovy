c862b9ad8829c7e58bf38c7ebee72ecb994563f1
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-04-17 18:53:26 +0900

    Added configuration part

--- asespotter2011.groovy
+++ asespotter2011.groovy
@@ -1,9 +1,32 @@
 /**
- * Main program.
- * TODO:
- * Group random pages query
- * Group revisions query for several pages
+ * ASE Spotter.
+ * See http://en.wikipedia.org/wiki/Wikipedia:ASE
+ * Author: Nicolas Raoul
  */
+
+/* ******************************** Configuration ******************************** */
+
+/**
+ * Number of revisions to consider for an article.
+ */
+REVISIONS_LIMIT = 50
+
+/**
+ * Number of edits after which an editor is considered experienced.
+ */
+EXPERIENCED_EDITCOUNT = 900
+
+/**
+ * Interval to wait between 2 requests to the server, in milliseconds.
+ */
+REQUESTS_INTERVAL = 300
+
+/* ******************************** Main script ******************************** */
+
+/**
+ * Main.
+ */
+System.setProperty("http.agent", "WikiProject ASE")
 spotAseArticles(10000)
 
 /**
@@ -12,18 +35,19 @@ spotAseArticles(10000)
 def spotAseArticles(numberOfArticles) {
         for ( i in 1..numberOfArticles ) {
 
-                // Get random page name
+                // Get random page name.
                 def node = new XmlSlurper().parse("http://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=xml")
                 def title = node.query.random.page.@title.toString()
 
-                // Skip if disambiguation
+                // Skip if disambiguation.
                 if ( title.contains("disambiguation") ) {
                         println "Disambiguation: " + title
                         continue
                 }
 
-                // Get editors of this page
-                node = new XmlSlurper().parse("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=" + title + "&rvprop=user&rvlimit=50&format=xml")
+                // Get editors of this page.
+                def urlencodedTitle = java.net.URLEncoder.encode(title)
+                node = new XmlSlurper().parse("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=" + urlencodedTitle + "&rvprop=user&rvlimit=" + REVISIONS_LIMIT + "&format=xml")
                 def revisions = node.query.pages.page.revisions.rev
                 println "Article with " + revisions.size() + " revisions: " + title
                 def editors = [] as Set
@@ -34,13 +58,12 @@ def spotAseArticles(numberOfArticles) {
                         }
                 }
 
-                // Ignore articles created by a bot
-                // If the last editor in the list (which chronologically means the first editor) is a bot, then continue
-                // False positive might appear for articles with more than 50 edits, which is not a big loss.
-                
-                //def lastEditor = revisions.list().last().@user.toString()
-                if ( ! revisions.list().isEmpty() && revisions.list().size() < 50 && isBot(/*lastEditor*/revisions.list().last().@user.toString()) ) {
-                        println "  Creator is a bot: " /*+ lastEditor*/
+                // Ignore articles created by a bot.
+                // If the last editor in the list (which chronologically means the first editor) is a bot, then continue.
+                // (only works for articles with less than 50 edits)
+                def lastEditor = revisions.list().last().@user.toString()
+                if ( revisions.list().size() < REVISIONS_LIMIT && isBot(lastEditor) ) {
+                        println "  Creator is a bot: " + lastEditor
                         continue
                 }
 
@@ -50,7 +73,7 @@ def spotAseArticles(numberOfArticles) {
                         def atLeastOneExperiencedEditor = false
                         for( editor in editors ) {
                                 // Wait a bit to reduce load on the local host and on the server.
-                                Thread.sleep(300)
+                                Thread.sleep(REQUESTS_INTERVAL)
                                 node = new XmlSlurper().parse("http://en.wikipedia.org/w/api.php?action=query&list=users&ususers=" + editor + "&usprop=editcount&format=xml")
                                 //println "                [DEBUG] editor=" + editor + ", editcount=" + node.query.users.user.@editcount.toString()
                                 def editcountString = node.query.users.user.@editcount.toString()
@@ -58,7 +81,7 @@ def spotAseArticles(numberOfArticles) {
                                 if ( editcountString.length() > 0) {
                                         editcount = Integer.parseInt(node.query.users.user.@editcount.toString())
                                 }
-                                if ( editcount > 900 ) {
+                                if ( editcount > EXPERIENCED_EDITCOUNT ) {
                                         println "  Editor experienced: " + editor + " (editcount:" + editcount + ")"
                                         atLeastOneExperiencedEditor = true
                                         break
