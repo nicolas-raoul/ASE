062951a00ecdebf706a221fabe6abfd4623044ae
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-05-12 15:57:14 +0900

    Fixed path

--- ase.sh
+++ ase.sh
@@ -1,3 +1,3 @@
-( groovy asespotter.groovy > ~/t/ase.txt ; grep "\[" ~/t/ase.txt ; zenity --info --text 'Done') &
+( groovy asespotter.groovy > ~/ase.txt ; grep "\[" ~/ase.txt ; zenity --info --text 'Done') &
 sleep 5
 tail -f ~/ase.txt
